a9b2021377964879acab261527808b115fa649da
Yuriy <akeno7@mail.ru>
2024-09-21 16:15:25 +0700

    duration selection

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-areas = 3
 PM = booking.PlaceManager()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -58,7 +57,7 @@ async def handle_book_room(update: Update, context: ContextTypes.DEFAULT_TYPE, r
         current_date += timedelta(days=1)
     keyboard = []
     for date in dates:
-        keyboard.append([InlineKeyboardButton(date, callback_data=f'ddroom_{room_id}_{date}')])
+        keyboard.append([InlineKeyboardButton(date, callback_data=f'd_room_{room_id}_{date}')])
     keyboard.append([InlineKeyboardButton("Back", callback_data="back")])
     reply_markup = InlineKeyboardMarkup(keyboard)        
     await update.callback_query.edit_message_text(f"{room_name} selected. Please choose a day:", reply_markup=reply_markup)
@@ -68,7 +67,7 @@ async def handle_book_time(update: Update, context: ContextTypes.DEFAULT_TYPE, r
     keyboard = []
     
     for i in range(0, len(slots), 5):
-        row = [InlineKeyboardButton(slot, callback_data=f'private_room_{room_id}_{date}_{slot}') for slot in slots[i:i+5]]
+        row = [InlineKeyboardButton(slot, callback_data=f'st_room_{room_id}_{date}_{slot}') for slot in slots[i:i+5]]
         keyboard.append(row)
 
     keyboard.append([InlineKeyboardButton("Back", callback_data="back")])
@@ -92,19 +91,19 @@ async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         city = query.data.split('_')[-1]
         await handle_book_city(update, context, city)
         
-    if query.data.startswith('ddroom_'):
+    if query.data.startswith('room_'):
+        room_id = query.data.split('_')[-1]
+        room_name = query.data.split('_')[-2]
+        await handle_book_room(update, context, room_id, room_name)   
+        
+    if query.data.startswith('d_room'):
         room_id = int(query.data.split("_")[-2])
         date = query.data.split("_")[-1]
         await handle_book_time(update, context, room_id, date) 
+    
+    if query.data.startswith('st_room'):
         
-    elif query.data.startswith('room_'):
-        room_id = query.data.split('_')[-1]
-        room_name = query.data.split('_')[-2]
-        await handle_book_room(update, context, room_id, room_name)
- 
         
-    # query.data.startswith("private_room_%d_"):
-   
 
     # if query.data    
 
