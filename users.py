b8f61dd5b28231abefccec74085205c9971c83a0
Yuriy <akeno7@mail.ru>
2024-09-21 23:13:08 +0700

    add users module

---
+++ users.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def db_connect():
+    return sqlite3.connect('users.db')
+
+def check_user_in_db(username, password):
+    conn = db_connect()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM tags WHERE tg_mail = ? AND password = ?', (username, password))
+    result = cursor.fetchone()
+
+    conn.close()
+    
+    return result is not None
